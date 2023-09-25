05e1b0757462738a930df7519f8f4d35bc88585d
Stev <stevenvallejo780@gmail.com>
2023-09-25 11:54:25 -0500

    ya cumple con todos los requisitos pero no son conclueyentes

--- teoria_sistemas.py
+++ teoria_sistemas.py
@@ -1,13 +1,9 @@
-# Mejorando el modelo y la visualización para abordar la Teoría de Sistemas de manera más completa
+# Manteniendo la estructura del código previo y continuando con las mejoras en visualización y análisis
 
 import random
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
-# Adaptación del código usando clases para una mejor claridad y dinámicas más realistas
-
-# Clase para Agentes con Homeostasis, No Linearidad, y Cooperación
 
 
 class Agent:
@@ -15,12 +11,12 @@ class Agent:
         self.money = money
         self.is_employer = is_employer
         self.previous_money = money
-        self.cooperation_index = cooperation_index  # Nuevo: índice de cooperación
+        self.cooperation_index = cooperation_index
 
     def work(self, market):
         earnings = (
             market.money * 0.01)**0.5 if self.is_employer else (market.money * 0.005)**0.5
-        self.money += earnings * self.cooperation_index  # Nuevo: efecto de la cooperación
+        self.money += earnings * self.cooperation_index
         market.money -= earnings
 
     def consume(self, market):
@@ -36,12 +32,10 @@ class Agent:
             self.is_employer = True
         self.previous_money = self.money
 
-    def cooperate(self, other):  # Nuevo: función de cooperación
+    def cooperate(self, other):
         cooperation_gain = self.money * 0.01 * self.cooperation_index
         self.money += cooperation_gain
         other.money += cooperation_gain
-
-# Clase para el Mercado con mecanismo de ajuste
 
 
 class Market:
@@ -51,13 +45,11 @@ class Market:
     def add_money(self, amount):
         self.money += amount
 
-    def adjust_conditions(self):  # Nuevo: ajuste del mercado
+    def adjust_conditions(self):
         if self.money < 2000:
             self.add_money(500)
         elif self.money > 8000:
             self.money -= 500
-
-# Clase para el Gobierno con jerarquía (Impuestos regionales y federales)
 
 
 class Government:
@@ -66,8 +58,8 @@ class Government:
         regional_tax = 0
 
         for agent in agents:
-            tax = agent.money * 0.07  # Impuesto federal
-            regional = agent.money * 0.03  # Impuesto regional
+            tax = agent.money * 0.07
+            regional = agent.money * 0.03
             agent.money -= (tax + regional)
             total_tax += tax
             regional_tax += regional
@@ -76,27 +68,22 @@ class Government:
             agent.money += (total_tax + regional_tax) / len(agents)
 
 
-# Parámetros e inicialización
 N = 100
 T = 500
 initial_market_money = 5000
 
-# Inicializar agentes y mercado con las clases corregidas
-agents = [Agent(money=random.randint(20, 100), is_employer=random.choice(
-    [True, False]), cooperation_index=random.uniform(0.8, 1.2)) for _ in range(N)]
+agents = [Agent(money=random.randint(20, 100), is_employer=random.choice([True, False]),
+                cooperation_index=random.uniform(0.8, 1.2)) for _ in range(N)]
 market = Market(initial_money=initial_market_money)
 government = Government()
 
-# Listas para rastrear la riqueza y satisfacción total
 total_employer_wealth = []
 total_employee_wealth = []
-total_satisfaction = []  # Nuevo: Satisfacción total en el sistema
+total_cooperation = []
 
-# Simulación con lógicas adaptadas
 for t in range(T):
     market.adjust_conditions()
 
-    # Cooperación entre agentes (Nuevo)
     for i in range(0, len(agents), 2):
         agents[i].cooperate(agents[i+1])
 
@@ -110,19 +97,23 @@ for t in range(T):
         sum(agent.money for agent in agents if agent.is_employer))
     total_employee_wealth.append(
         sum(agent.money for agent in agents if not agent.is_employer))
-    total_satisfaction.append(
-        sum(agent.money * agent.cooperation_index for agent in agents))  # Nuevo
+    total_cooperation.append(
+        sum(agent.cooperation_index for agent in agents)/N)
 
-# Gráfico 3D para visualizar la dinámica en el sistema
+# 3D plot for enhanced visualization
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(range(T), total_employer_wealth, total_satisfaction,
-        label='Employer Wealth vs Satisfaction')
-ax.plot(range(T), total_employee_wealth, total_satisfaction,
-        label='Employee Wealth vs Satisfaction')
-ax.set_xlabel('Turn')
-ax.set_ylabel('Total Wealth')
-ax.set_zlabel('Total Satisfaction')
+
+x = np.linspace(1, T, T)
+ax.plot(x, total_employer_wealth, zs=0,
+        zdir='z', label='Total Employer Wealth')
+ax.plot(x, total_employee_wealth, zs=1,
+        zdir='z', label='Total Employee Wealth')
+ax.plot(x, total_cooperation, zs=2, zdir='z', label='Avg Cooperation Index')
+
+ax.set_xlabel('Time')
+ax.set_ylabel('Z-Direction')
+ax.set_zlabel('Wealth/Cooperation')
 ax.legend()
-plt.title('Advanced System Dynamics')
+plt.title('Adapted System Dynamics with 3D Visualization')
 plt.show()
