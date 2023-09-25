9e25f6a0aea7b5384ea7fdb3a4d114d90c57f307
Stev <stevenvallejo780@gmail.com>
2023-09-25 12:18:42 -0500

    logrado pero refinable

--- teoria_sistemas.py
+++ teoria_sistemas.py
@@ -1,9 +1,10 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
-# Class for Agents with Homeostasis, Non-linearity, and Cooperation
+# Class for Agents with Homeostasis, Non-linearity, Cooperation and Competition
 
 
 class Agent:
@@ -14,7 +15,7 @@ class Agent:
         self.cooperation_index = cooperation_index
 
     def work(self, market):
-        # Introduciendo competencia: los agentes compiten por un recurso limitado en el mercado
+        # Introducing competition: agents compete for a limited resource in the market
         available_resource = market.money * \
             0.01 if self.is_employer else market.money * 0.005
         earnings = min((available_resource)**0.5, self.money * 0.1)
@@ -38,6 +39,11 @@ class Agent:
         cooperation_gain = self.money * 0.01 * self.cooperation_index
         self.money += cooperation_gain
         other.money += cooperation_gain
+
+    def compete(self, other):  # New: competition function
+        if self.money > other.money:
+            self.money += 10
+            other.money -= 10
 
 # Class for the Market with adjustment mechanism
 
@@ -94,9 +100,13 @@ total_satisfaction = []
 for t in range(T):
     market.adjust_conditions()
 
+    # Competition between agents
+    for i in range(0, len(agents), 2):
+        agents[i].compete(agents[i + 1])
+
     # Cooperation between agents
     for i in range(0, len(agents), 2):
-        agents[i].cooperate(agents[i+1])
+        agents[i].cooperate(agents[i + 1])
 
     for agent in agents:
         agent.work(market)
@@ -104,7 +114,7 @@ for t in range(T):
         agent.adapt()
     government.tax_and_redistribute(agents)
 
-    # Tracking total wealth
+    # Tracking total wealth and satisfaction
     total_employer_wealth.append(
         sum(agent.money for agent in agents if agent.is_employer))
     total_employee_wealth.append(
@@ -112,29 +122,18 @@ for t in range(T):
     total_satisfaction.append(
         sum(agent.money * agent.cooperation_index for agent in agents))
 
-# 3D Plot for better visualization
-fig = plt.figure(figsize=(12, 6))
-ax1 = fig.add_subplot(121, projection='3d')
-ax1.plot(range(T), total_employer_wealth, zs=0,
-         zdir='y', label='Total Employer Wealth')
-ax1.plot(range(T), total_employee_wealth, zs=20,
-         zdir='y', label='Total Employee Wealth')
-ax1.plot(range(T), total_satisfaction, zs=40,
-         zdir='y', label='Total Satisfaction')
-ax1.set_xlabel('Turn')
-ax1.set_ylabel('Metrics')
-ax1.set_zlabel('Total Wealth/Satisfaction')
-ax1.set_title('3D Visualization of System Dynamics')
-ax1.legend()
-
-# Heatmap for better visualization
-ax2 = fig.add_subplot(122)
-data = np.array(
-    [total_employer_wealth, total_employee_wealth, total_satisfaction])
-sns.heatmap(data, annot=False, ax=ax2, cmap="YlGnBu")
-ax2.set_title('Heatmap of System Dynamics')
-ax2.set_yticklabels(
-    ['Total Employer Wealth', 'Total Employee Wealth', 'Total Satisfaction'])
-ax2.set_xticklabels([])
-
+# 3D Visualization
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(range(T), total_employer_wealth, zs=0,
+        zdir='z', label='Total Employer Wealth')
+ax.plot(range(T), total_employee_wealth, zs=1,
+        zdir='z', label='Total Employee Wealth')
+ax.plot(range(T), total_satisfaction, zs=2,
+        zdir='z', label='Total Satisfaction')
+ax.set_xlabel('Turn')
+ax.set_ylabel('Z-axis')
+ax.set_zlabel('Total Wealth/Satisfaction')
+plt.title('System Dynamics with Competition and Cooperation')
+plt.legend()
 plt.show()
